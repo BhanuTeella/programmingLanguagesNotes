8988db1f994fcd24bd2ced318e23c6ff27c75fd7
Bhanu Teella <83585623+BhanuTeella@users.noreply.github.com>
2023-07-17 17:43:17 +0530

    added files

--- java programs/helloWorld.java
+++ java programs/helloWorld.java
@@ -1,5 +1,5 @@
-public class helloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+public class helloWorld {// class name should be same as file name, public is access modifier
+    public static void main(String[] args) {// main method, string array is parameter, static allows to call method without creating object
+        System.out.println("Hello, world!");//system is a class, out is a public static field, println is a method
     }
 }
