90841d04d110ce721c44206097057c92b9df97cd
Bhanu Teella <83585623+BhanuTeella@users.noreply.github.com>
2023-07-18 17:00:59 +0530

    added files

---
+++ BasicInputOutput.js
@@ -0,0 +1,6 @@
+
+//console.log is basic type of output. it is like print 6in python
+console.log(name)
+console.log("Hello "+name+" Welcome to Javascript")
+
+// javascript input is basically in the context of web browser. if we still want to take input from user we can use readline module in nodejs. But it is complex
