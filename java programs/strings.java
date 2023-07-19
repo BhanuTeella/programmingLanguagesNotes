79afdac01bf20c1335a2fa909dc8dd440ad04f0a
Bhanu Teella <83585623+BhanuTeella@users.noreply.github.com>
2023-07-19 20:24:02 +0530

    added string.java

--- java programs/strings.java
+++ java programs/strings.java
@@ -1,3 +1,21 @@
 public class strings {
-    
+    public static void main(String[] args){
+        //String is a built-in class in java
+        //String is immutable in java
+        //String is a sequence of characters. But it is not an array of characters
+        
+        //String declaration
+        String name="b";
+        System.out.println("hello!"+name);
+
+        //Accessing individual characters
+        System.out.println(name.charAt(0));//prints h
+
+        //Concatenating strings
+        String str1="Hello";
+        String str2="World";
+        String str3=str1+" "+str2;
+        System.out.println(str3);
+
+    }
 }
