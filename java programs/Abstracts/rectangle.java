6a3cd48f86d5c1c0ec107f4de6b4bc4a2b569a0f
Bhanu Teella <83585623+BhanuTeella@users.noreply.github.com>
2023-07-20 22:23:27 +0530

    added files

---
+++ java programs/Abstracts/rectangle.java
@@ -0,0 +1,21 @@
+package Abstracts;
+
+
+public class rectangle extends shape {
+    private double ln, br;
+
+    public rectangle(double a, double b) {
+        ln = a;
+        br = b;
+    }
+
+    public double perimeter() {
+        return 2 * (ln + br);
+    }
+
+    public double area() {
+        return ln * br;
+    }
+
+   
+}
